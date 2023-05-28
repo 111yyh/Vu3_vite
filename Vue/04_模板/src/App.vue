3c45c24e0a3b4e325c9f26ee4ca2766e0746459f
111yyh <1145986635@qq.com>
2023-05-28 20:42:38 +0800

    lilichao review

--- Vue/04_模板/src/App.vue
+++ Vue/04_模板/src/App.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-    <h1>hello</h1>
+    <h1>1111</h1>
 </template>
 
-<style>
-
+<style scoped>
+    
 </style>
---
+++ Vue/05_练习/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue';
+  // 0 球员 1 球队
+  const current = ref(0)
+
+</script>
+
+<template>
+  <!-- 选项卡的外部容器 -->
+  <div class="tab-wrapper">
+    <!-- 选项卡的头部 -->
+    <header class="tab-head">
+      <div @click="current = 0" class="tab-button" :class="{active:current === 0}">热门球员</div>
+      <div @click="current = 1" class="tab-button" :class="{active:current === 1}">热门球队</div>
+    </header>
+    <!-- 选项卡的主体 -->
+    <div class="main"></div>
+  </div>
+</template>
+
+<style scoped>
+  .tab-wrapper {
+    width: 800px;
+    box-sizing: border-box;
+    padding: 20px;
+    background-color: rgb(45, 83, 211);
+  }
+  .tab-head {
+    display: flex;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+  .tab-button {
+    background-color: #fff;
+    font-size: 30px;
+    padding: 10px 0;
+    text-align: center;
+    cursor: pointer;
+    flex:auto;
+    transition: 0.2s;
+  }
+  .tab-button:not(.active):hover {
+    color:rgb(187, 3, 52);
+  }
+  .active{
+    background-color: rgb(187, 3, 52);
+    color: #fff;
+  }
+</style>
